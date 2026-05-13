8fba6e57f8e7df1580582e5a552d97a7a81c3349
GustavoLenni <gustavogithub1@gmail.com>
2026-05-12 21:32:26 -0300

    Quicksort correct

--- Ordenation/QuickSort.java
+++ Ordenation/QuickSort.java
@@ -2,27 +2,39 @@ package Ordenation;
 
 public class QuickSort {
     public static void main(String[] args){
-        int [] arr = {8,65,1,2,6};
+        int arr[] = {2,4,5,9,8};
 
-        int pivot = arr[arr.length-1];
-        int index_border = -1;
+        quicksort(arr,0, arr.length-1);
 
-        for(int i = 0; i < arr.length -1;i++){
+        for(int i = 0; i < arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void quicksort(int arr[], int low, int high){
+        if(low < high){
+            int pivotIndex = partition(arr,low,high);
+
+            quicksort(arr, low,pivotIndex -1);
+            quicksort(arr, pivotIndex + 1, high);
+        }
+    }
+    public static int partition(int arr[], int low, int high){
+        int pivot = arr[high];
+        int index_border = low - 1;
+
+        for(int i = low; i < high; i++ ){
             if(arr[i] < pivot){
                 index_border++;
-                int temp = arr[i];
+
+                int temp =  arr[i];
                 arr[i] = arr[index_border];
                 arr[index_border] = temp;
             }
         }
         int temp = arr[index_border+1];
-        arr[index_border+1] = pivot; // correct position pivot
-        arr[arr.length-1] = temp; // oldest position of pivot
+        arr[index_border+1] = arr[high];
+        arr[high] = temp;
 
-        for(int result = 0; result < arr.length; result++){
-            System.out.println(arr[result]);
-        }
+        return index_border + 1;
     }
-
-
 }
